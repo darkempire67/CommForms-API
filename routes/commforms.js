077eafdb31eea225576c1ece1383f7fe870348fe
Cristofer Ramos Guzman <crisrguzman@DarkEmpire.local>
2020-10-22 05:59:57 -0700

    comment

--- routes/commforms.js
+++ routes/commforms.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const CommForm = require("../models/CommForm");
 
-// routes
+// routes get all
 
 router.get("/", async (req, res) => {
   try {
